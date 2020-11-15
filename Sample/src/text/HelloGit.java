8c613597c3f5fcc04ffe0e350a884ec6f78ddd46
venkatsai007 <venkatsai007@Deepak>
2020-11-15 16:12:44 +0530

    first commit

---
+++ Sample/src/text/HelloGit.java
@@ -0,0 +1,9 @@
+package text;
+
+public class HelloGit {
+public static void main(String[] args) {
+	System.out.print("Demo git project");
+	System.out.println("from eclipse");
+	System.out.println("Example of Abridged batch-fourt commit");
+}
+}
