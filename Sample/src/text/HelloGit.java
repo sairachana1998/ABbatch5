1f722617107044b3d0ab371b10d425a314561a22
venkatsai007 <venkatsai007@Deepak>
2020-11-17 10:18:58 +0530

    second commit

--- Sample/src/text/HelloGit.java
+++ Sample/src/text/HelloGit.java
@@ -5,5 +5,7 @@ public static void main(String[] args) {
 	System.out.print("Demo git project");
 	System.out.println("from eclipse");
 	System.out.println("Example of Abridged batch-fourt commit");
+	System.out.println("hello");
+	
 }
 }
