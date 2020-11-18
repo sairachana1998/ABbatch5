435185ea843a007cfe3e91fd4f1d1f62b1317eba
venkatsai007 <venkatsai007@Deepak>
2020-11-18 15:06:26 +0530

    edited by rachana

---
+++ batch5/src/main/java/com/insure/quote/dao/Sample.java
@@ -0,0 +1,7 @@
+package com.insure.quote.dao;
+
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
---
+++ batch5/src/main/java/com/insure/quote/testcase/Sample.java
@@ -0,0 +1,7 @@
+package com.insure.quote.testcase;
+
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
